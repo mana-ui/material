d640f401a1de109f9251b43ed9a953b21e31936e
qdl <levinqdl@magicengine.com.cn>
2021-06-17 11:09:01 +0800

    outlined button

--- src/Button.js
+++ src/Button.js
@@ -36,4 +36,11 @@ const Text = styled(Button)`
   }
 `
 
-export {Button, Text};
+const Outlined = styled(Text)`
+  border-style: solid;
+  padding: 0 15px 0 15px;
+  border-width: 1px;
+  border-color: ${props => props.theme.color.primary}
+`
+
+export {Button, Text, Outlined};
