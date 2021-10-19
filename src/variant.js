0e5632211ff18456bc53bdddb3330c270e911f91
qdl <levinqdl@magicengine.com.cn>
2021-10-19 10:54:17 +0800

    change variant to functions

--- src/variant.js
+++ src/variant.js
@@ -1,8 +1,8 @@
 import Color from "color";
 
 const variant = (theme) => {
-	theme.color.brighter ??= Color(theme.color.primary).fade(0.08).string()
-	theme.color.highlight ??= Color(theme.color.primary).fade(0.96).string()
+	theme.color.brighter = (color = theme.color.primary) => Color(color).fade(0.08).string()
+	theme.color.highlight = (color = theme.color.primary) => Color(color).fade(0.96).string()
 	return theme
 }
 
